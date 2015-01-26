a923d0ed90877720b47e9584cd7bc87d80c24303
Vitali V <vitali.vasilioglo@qundoo.com>
2015-01-27 00:40:20 +0100

    license & name updated

---
+++ src/main/groovy/com/vv/testy/TestyPlugin.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015 Vitali Vasilioglo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.vv.testy
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import com.android.builder.testing.ConnectedDeviceProvider
+
+class TestyPlugin implements Plugin<Project> {
+    void apply(Project project) {
+		def DEVICES_PROPERTY = "devices"
+        ConnectedDeviceProvider.metaClass.getDevices = {
+            def devices = localDevices
+            if (project.hasProperty(DEVICES_PROPERTY)){
+                def devicesSerials = project.getProperties().get(DEVICES_PROPERTY).split(/,/)
+                println("running tests on: ${devicesSerials}")
+                devices = localDevices.findAll { devicesSerials.contains(it.serialNumber) }
+            }
+
+            devices
+        }
+    }
+}
